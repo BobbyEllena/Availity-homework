aea5ab46327103b314e4a0deb7022314ad467760
Bobby Ellena <bobbyellena@gmail.com>
2021-10-23 10:07:21 -0500

    first commit

---
+++ questions/4/question4.py
@@ -0,0 +1,7 @@
+# Coding exercise: You are tasked to write a checker that 
+# validates the parentheses of a LISP code.  Write a program 
+# (in Node.js or JavaScript) which takes in a string as an 
+# input and returns true if all the parentheses in the string 
+# are properly closed and nested.
+
+
