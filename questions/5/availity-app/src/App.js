16b6a2319761b67087981afb79da0899863a3388
Bobby Ellena <bobbyellena@gmail.com>
2021-10-24 19:56:05 -0500

    finished the form

--- questions/5/availity-app/src/App.js
+++ questions/5/availity-app/src/App.js
@@ -9,32 +9,73 @@
 //   Telephone Number
 //   Email address
 
+import React, { useReducer, useState } from 'react';
 import './App.css';
 
+const formReducer = (state, event) => {
+  return {
+    ...state,
+    [event.name]: event.value
+  }
+}
+
 function App() {
-  const handleEvent = event => {
-    event.preventDefault()
-    alert("You are not registered.")
+  const [formData, setFormData] = useReducer(formReducer, {});
+  const [submitting, setSubmitting] = useState(false);
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    setSubmitting(true);
+
+    setTimeout(() => {
+      setSubmitting(false);
+    }, 3000);
   }
 
-  return (
+  const handleChange = event => {
+    setFormData({
+      name: event.target.name,
+      value: event.target.value,
+    });
+  }
+
+  return(
     <div className="App">
-        <p>
-          <h1>Welcome</h1>
-          <form>
-            <fieldset>
-              <label>                
-                <p>Name</p>  
-                <input name="Name" />
-              </label>
-            </fieldset>
-            <button type="submit">Submit</button>
-          </form>
-        </p>
-      
-      
+      <h1>Welcome</h1>
+      {submitting &&
+        <div>Submtting Form...</div>
+      }
+      <form onSubmit={handleSubmit}>
+        <fieldset>
+          <label>
+            <p>First Name</p>
+            <input name="first_name" onChange={handleChange}/>
+          </label>
+          <label>
+            <p>Last Name</p>
+            <input name="last_name" onChange={handleChange}/>
+          </label>
+          <label>
+            <p>NPI Number</p>
+            <input name="npi_number" onChange={handleChange}/>
+          </label>
+          <label>
+            <p>Business Address</p>
+            <input name="business_address" onChange={handleChange}/>
+          </label>
+          <label>
+            <p>Telephone Number</p>
+            <input name="telephone_number" onChange={handleChange}/>
+          </label>
+          <label>
+            <p>Email Address</p>
+            <input name="email_address" onChange={handleChange}/>
+          </label>
+        </fieldset>
+        <button type="submit">Submit</button>
+      </form>
     </div>
-  );
+  )
 }
 
 export default App;
